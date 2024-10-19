1e4a7e90c6d1b8b9e9e85f5b9f00d667fe7a35e2
Miranda990 <168492604+Miranda990@users.noreply.github.com>
2024-10-18 22:18:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿char sexo;
+double altura, peso;
+
+Console.Clear();
+
+
+Console.WriteLine("Voce é [M] = masculino ou [F] = feminino:...");
+sexo = Convert.ToChar(Console.ReadLine().ToUpper());
+Console.Write("Coloque aqui sua altura......");
+altura = Convert.ToDouble(Console.ReadLine());
+
+if (sexo != 'M' && sexo != 'F')
+{
+    Console.WriteLine("Precisa ser Masculino ou Feminino!");
+}
+else 
+{
+    if(sexo == 'M')
+    {
+        peso = altura * 72.7 - 58.0;
+    }
+    else
+    {
+        peso = altura * 62.1 - 44.7;
+    }
+    Console.WriteLine($"Seu peso é {peso:N1}!");
+}
+
